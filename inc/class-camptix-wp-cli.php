bd912da27ad9f0476cc0d2c435533107ba3f6bc5
Nikolay Bachiyski <nb@nikolay.bg>
2012-12-10 12:37:34 +0200

    Add support for TEDxBG CSV

--- inc/class-camptix-wp-cli.php
+++ inc/class-camptix-wp-cli.php
@@ -1,7 +1,6 @@
 <?php
 
 class Camptix_Command extends WP_CLI_Command {
-	const DEFAULT_QUANTITY = 1;
 
 	/**
 	 * @synposis ticket-post-id csv-filename
@@ -18,10 +17,15 @@ class Camptix_Command extends WP_CLI_Command {
 			WP_CLI::error( "Error opening CSV file '$file_name'." );
 		}
 		$reservations = array();
-		while ( ( $entry = fgetcsv( $csv ) ) !== false ) {
-			$reservations[] = array( 'name' => trim( $entry[0] ), 'quantity' => isset( $entry[1] )? intval( $entry[1] ) : self::DEFAULT_QUANTITY );
+		while ( ( $entry = fgetcsv( $csv, 1000, "\t" ) ) !== false ) {
+			$reservations[] = array( 'name' => trim( $entry[0] ), 'email' => trim( $entry[1] ), 'quantity' => isset( $entry[4] )? ( $entry[4] ): ( intval( $entry[3] )? $entry[3] : 0 ) );
 		}
 		foreach( $reservations as $reservation ) {
+			if ( !$reservation['name'] || !$reservation['quantity'] ) continue;
+			if ( !$reservation['email'] ) {
+				WP_CLI::line( "Skipping {$reservation['name']}, because it doesn't have an e-mail." );
+				continue;
+			}
 			WP_CLI::line( "Creating a reservation for '{$reservation['name']}' for {$reservation['quantity']} tickets." );
 			$camptix->create_reservation( $post_id, $reservation['name'], $reservation['quantity'] );
 		}
