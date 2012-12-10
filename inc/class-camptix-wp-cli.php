13fbd9f0d5bb0815b0cfb5a00931a654e3d69067
Nikolay Bachiyski <nb@nikolay.bg>
2012-12-10 12:18:10 +0200

    Revert "Make add-reservation use separate names/quantities files"
    Will go with CSV in the end.
    
    This reverts commit 5f3406e4c41acc09ddcd3c7171bca91c474cf60e.

--- inc/class-camptix-wp-cli.php
+++ inc/class-camptix-wp-cli.php
@@ -4,37 +4,23 @@ class Camptix_Command extends WP_CLI_Command {
 	const DEFAULT_QUANTITY = 1;
 
 	/**
-	 * @synposis ticket-post-id names-filename quantities-filename
+	 * @synposis ticket-post-id csv-filename
 	 * @subcommand add-reservations
 	 */
 	function add_reservations( $args, $assoc_args ) {
 		global $camptix;
-		list( $post_id, $names_file_name, $quantities_file_name ) = $args;
+		list( $post_id, $file_name ) = $args;
 		if ( 'tix_ticket' != get_post_type( $post_id ) ) {
 			WP_CLI::error( "There's no ticket with post_id '$post_id'." );
 		}
-		$names_text = file_get_contents( $names_file_name );
-		if ( false === $names_text ) {
-			WP_CLI::error( "Error reading names from '$names_file_name'." );
+		$csv = fopen( $file_name, 'r' );
+		if ( !$csv ) {
+			WP_CLI::error( "Error opening CSV file '$file_name'." );
 		}
-		$quantities_text = file_get_contents( $quantities_file_name );
-		if ( false === $quantities_text ) {
-			WP_CLI::error( "Error reading quantities from '$quantities_file_name'." );
-		}
-		$names = split( "\n", $names_text );
-		$quantities = split( "\n", $quantities_text );
-
-		if ( count( $names ) != count( $quantities ) ) {
-			WP_CLI::error( "Names and quantities have different counts." );
-		}
-
 		$reservations = array();
-		for( $i = 0; $i < count( $names ); ++$i ) {
-			if ( !$names[$i] && !$quantities[$i] )
-				continue;
-			$reservations[] = array( 'name' => trim( $names[$i] ), 'quantity' => intval( $quantities[$i] ) );
+		while ( ( $entry = fgetcsv( $csv ) ) !== false ) {
+			$reservations[] = array( 'name' => trim( $entry[0] ), 'quantity' => isset( $entry[1] )? intval( $entry[1] ) : self::DEFAULT_QUANTITY );
 		}
-
 		foreach( $reservations as $reservation ) {
 			WP_CLI::line( "Creating a reservation for '{$reservation['name']}' for {$reservation['quantity']} tickets." );
 			$camptix->create_reservation( $post_id, $reservation['name'], $reservation['quantity'] );
